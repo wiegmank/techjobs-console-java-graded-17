9934f894874d097c62d77abb955bb22c956dff81
Kevin Wiegman <wiegmank@gmail.com>
2023-12-09 10:51:39 -0600

    task 1 & 3 complete, maybe task 2

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -95,7 +92,18 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            //for each row in "allJobs"
+            if (row.containsValue(value)) {
+                jobs.add(row);
+            }
+            //if any cell contains value of "value", add that row (1x only)
+            }
+
+        return jobs;
     }
 
     /**
